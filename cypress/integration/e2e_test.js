7978da5ea78f5c9af3415f437bc6185ea0fb0341
stvbex <12851562+stvbex@users.noreply.github.com>
2021-01-18 14:48:20 +0200

    second e2e test

--- cypress/integration/e2e_test.js
+++ cypress/integration/e2e_test.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('beedrill').click()
+    cy.contains('swarm')
+    cy.contains('sniper')
+
+  })
 })
